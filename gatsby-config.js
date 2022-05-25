6956ded9e231b8c4f31d8d28c7b748d196f68b63
PinetheApple <jonathansibypaul@gmail.com>
2022-05-25 17:21:31 +0530

    fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,34 @@
 module.exports = {
-    siteMetadata: {
-      title: "Test Site",
-      siteUrl: `https://www.gatsbyjs.com`,
-      description: "This is where I write my thoughts.",
-      image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
-    },
-    plugins: ['gatsby-plugin-react-helmet'],
-    
-  }
+  siteMetadata: {
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Frontend Masters Intro to Gatsby',
+    description: 'Frontend Masters Intro to Gatsby course project',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+     {
+       resolve: 'gatsby-source-filesystem',
+       options: {
+         name: 'posts',
+         path: `${__dirname}/src/posts/`,
+       },
+     },
+     {
+       resolve: 'gatsby-plugin-page-creator',
+       options: {
+         path: `${__dirname}/src/posts/`,
+       },
+     },
+     {
+       resolve: 'gatsby-plugin-mdx',
+       options: {
+         defaultLayouts: {
+           posts: require.resolve('./src/components/layout.js'),
+         },
+       },
+     },
+  ],
+};
   
